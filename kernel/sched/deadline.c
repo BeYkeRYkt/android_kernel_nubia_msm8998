95e63b82aa8139afbe9c940dc95dbc14528593c0
Wanpeng Li <wanpeng.li@hotmail.com>
2016-08-04 09:42:20 +0800

    sched/deadline: Fix lock pinning warning during CPU hotplug
    The following warning can be triggered by hot-unplugging the CPU
    on which an active SCHED_DEADLINE task is running on:
    
      WARNING: CPU: 0 PID: 0 at kernel/locking/lockdep.c:3531 lock_release+0x690/0x6a0
      releasing a pinned lock
      Call Trace:
       dump_stack+0x99/0xd0
       __warn+0xd1/0xf0
       ? dl_task_timer+0x1a1/0x2b0
       warn_slowpath_fmt+0x4f/0x60
       ? sched_clock+0x13/0x20
       lock_release+0x690/0x6a0
       ? enqueue_pushable_dl_task+0x9b/0xa0
       ? enqueue_task_dl+0x1ca/0x480
       _raw_spin_unlock+0x1f/0x40
       dl_task_timer+0x1a1/0x2b0
       ? push_dl_task.part.31+0x190/0x190
      WARNING: CPU: 0 PID: 0 at kernel/locking/lockdep.c:3649 lock_unpin_lock+0x181/0x1a0
      unpinning an unpinned lock
      Call Trace:
       dump_stack+0x99/0xd0
       __warn+0xd1/0xf0
       warn_slowpath_fmt+0x4f/0x60
       lock_unpin_lock+0x181/0x1a0
       dl_task_timer+0x127/0x2b0
       ? push_dl_task.part.31+0x190/0x190
    
    As per the comment before this code, its safe to drop the RQ lock
    here, and since we (potentially) change rq, unpin and repin to avoid
    the splat.
    
    Change-Id: I78d6f910d1d84651a15bc674eaf7d690632c3fdf
    Signed-off-by: Wanpeng Li <wanpeng.li@hotmail.com>
    [ Rewrote changelog. ]
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@arm.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Luca Abeni <luca.abeni@unitn.it>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1470274940-17976-1-git-send-email-wanpeng.li@hotmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -780,7 +780,9 @@ static enum hrtimer_restart dl_task_timer(struct hrtimer *timer)
 	 * XXX figure out if select_task_rq_dl() deals with offline cpus.
 	 */
 	if (unlikely(!rq->online)) {
+		lockdep_unpin_lock(&rq->lock, rf.cookie);
 		rq = dl_task_offline_migration(rq, p);
+		rf.cookie = lockdep_pin_lock(&rq->lock);
 		update_rq_clock(rq);
 	}
 
