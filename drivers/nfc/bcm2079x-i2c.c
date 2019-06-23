62eb8ba8f3a33f8d96cba0b37df070e21ce4abb5
Michael Bestas <mkbestas@lineageos.org>
2019-06-23 03:15:58 +0300

    nfc: bcm2079x: Convert to wakeup sources
    Change-Id: I308ea2f751941333cbd113d8fe66fe6dd6c657b2

--- drivers/nfc/bcm2079x-i2c.c
+++ drivers/nfc/bcm2079x-i2c.c
@@ -41,7 +41,6 @@
 #include <linux/nfc/bcm2079x.h>
 #include <linux/of_gpio.h>
 #include <linux/syscalls.h>
-#include <linux/wakelock.h>
 
 /* end of compile options */
 
@@ -69,7 +68,7 @@ struct bcm2079x_dev_t {
 	bool irq_enabled;
 	spinlock_t irq_enabled_lock;
 	unsigned int count_irq;
-	struct wake_lock wakelock;
+	struct wakeup_source wakelock;
 };
 
 static struct clk *clk_rf;
@@ -166,7 +165,7 @@ static irqreturn_t bcm2079x_dev_irq_handler(int irq, void *dev_id)
 	spin_lock_irqsave(&bcm2079x_dev->irq_enabled_lock, flags);
 	bcm2079x_dev->count_irq++;
 	spin_unlock_irqrestore(&bcm2079x_dev->irq_enabled_lock, flags);
-	wake_lock_timeout(&bcm2079x_dev->wakelock, HZ);
+	__pm_wakeup_event(&bcm2079x_dev->wakelock, HZ);
 	dev_info(&bcm2079x_dev->client->dev,"bcm2079x_dev_irq_handler, count_irq = %d\n", bcm2079x_dev->count_irq);
 	wake_up(&bcm2079x_dev->read_wq);
 
@@ -568,7 +567,7 @@ static int bcm2079x_probe(struct i2c_client *client,
 	bcm2079x_dev->en_gpio = platform_data->en_gpio;
 	bcm2079x_dev->client = client;
 
-	wake_lock_init(&bcm2079x_dev->wakelock, WAKE_LOCK_SUSPEND, "bcm2079x");
+	wakeup_source_init(&bcm2079x_dev->wakelock, "bcm2079x");
 
 	/* init mutex and queues */
 	init_waitqueue_head(&bcm2079x_dev->read_wq);
@@ -619,8 +618,8 @@ err_request_irq_failed:
 	misc_deregister(&bcm2079x_dev->bcm2079x_device);
 err_misc_register:
 	mutex_destroy(&bcm2079x_dev->read_mutex);
-	wake_unlock(&bcm2079x_dev->wakelock);
-	wake_lock_destroy(&bcm2079x_dev->wakelock);
+	__pm_relax(&bcm2079x_dev->wakelock);
+	wakeup_source_trash(&bcm2079x_dev->wakelock);
 
 	kfree(bcm2079x_dev);
 err_exit:
@@ -645,8 +644,8 @@ static int bcm2079x_remove(struct i2c_client *client)
 	gpio_free(bcm2079x_dev->irq_gpio);
 	gpio_free(bcm2079x_dev->en_gpio);
 	gpio_free(bcm2079x_dev->wake_gpio);
-	wake_unlock(&bcm2079x_dev->wakelock);
-	wake_lock_destroy(&bcm2079x_dev->wakelock);
+	__pm_relax(&bcm2079x_dev->wakelock);
+	wakeup_source_trash(&bcm2079x_dev->wakelock);
 	bcm2079x_clk_disable();
 	kfree(bcm2079x_dev);
 
